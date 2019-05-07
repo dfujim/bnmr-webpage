be797a72d7590c0c06f3bda5b118c28cd8ac998c
Derek Fujimoto <fujimoto@phas.ubc.ca>
2019-05-07 12:08:37 -0700

    Updated for use with bib files

--- bibliography.py
+++ bibliography.py
@@ -1,24 +1,78 @@
 # formatted using black
 
 import datetime
+import bibtexparser
+import os
+import pandas as pd
 
+class bib_collection(object):
+    """
+        Holds all bib objects corresponding to a bibtex file
+        
+        data: dictionary of bib objects
+    """
+    
+    def __init__(self,bibfile,notes=''):
+        """
+            bibfile:    path to bibtex file. Name is applied to "kind" input to 
+                        bibobject
+            notes:      file with keys referencing bibfile. Applies openaccess 
+                        and note fields. csv: key,openaccess,note
+        """
+        
+        # check file 
+        if not os.path.isfile(bibfile):
+            raise IOError('File not found')
+        
+        # open file 
+        with open(bibfile,'r') as fid: 
+            lines = fid.readlines()
+            
+        # get rid of commented lines
+        lines = [l for l in lines if l.strip() and '%' != l.strip()[0]]
+        
+        # get rid of mid-line comments
+        for i,l in enumerate(lines):
+            j = l.find('%')
+            if l[j-1] != '\\':
+                lines[i] = l[:j]
+            
+        # get list of inputs 
+        entries = bibtexparser.loads('\n'.join(lines)).entries_dict
+        
+        # get kind
+        kind = os.path.basename(os.path.splitext(bibfile)[0])
+        for k in entries:   entries[k]['kind'] = kind
+        
+        # get notes
+        if notes:
+            df = pd.read_csv(notes)
+            df.set_index('key',inplace=True)
+            
+            for k in entries:
+                if k in df.index:
+                    access = False if df.loc[k,'openaccess'].upper() == 'FALSE' else True 
+                    entries[k]['openaccess'] = access
+                    entries[k]['note'] = df.loc[k,'note']
+            
+        # make bib objects
+        self.data = {k:bib(**(entries[k])) for k in entries}
 
-class bib:
-    """ a simple class for holding bibliography entry info """
+class bib(object):
+    """ A simple class for holding bibliography entry info """
 
-    """ debatably a much dumber re-implementation bibtex, but whatever... """
-
-    """ icons """
+    # icons 
     icon_doi = '<i class="ai ai-doi"></i>'
     icon_arxiv = '<i class="ai ai-arxiv"></i>'
     icon_orcid = '<i class="ai ai-orcid"></i>'
-    icon_openacess = '<i class="ai ai-open-access" style="color:orangered"></i>'
+    icon_openaccess = '<i class="ai ai-open-access" style="color:orangered"></i>'
 
-    """ base urls """
+    # base urls
     urlbase_doi = "https://doi.org/"
     urlbase_arxiv = "https://arxiv.org/abs/"
 
-    def __init__(self, **kwargs):
+    def __init__(self,**kwargs):
+            
         """ type of entry (article, proceeding, preprint, thesis) """
         self.kind = kwargs.get("kind", None)
 
@@ -32,68 +86,40 @@ class bib:
         self.volume = kwargs.get("volume", None)
         self.number = kwargs.get("number", None)
         self.pages = kwargs.get("pages", None)
-        self.year = kwargs.get("year", "0001")
-
-        """ (legacy) date info - fails when journal year/pub_year don't match"""
-        self.month = kwargs.get("month", "01")
-        self.day = kwargs.get("day", "01")
 
         """ digital object identifiers """
         self.doi = kwargs.get("doi", None)
         self.url = kwargs.get("url", None)
 
+        """ openaccess """
+        self.openaccess = kwargs.get("openaccess", False)
+        
         """ arxiv preprints identifiers """
         self.arxiv = kwargs.get("arxiv", None)
         self.arxiv_cat = kwargs.get("arxiv_cat", None)
-
-        """ open access article flag """
-        self.openacess = kwargs.get("openacess", False)
-
+        
         """ thesis fields """
-        self.degree = kwargs.get("degree", None)
+        entry_type = kwargs.get("ENTRYTYPE", None)
+        if 'mastersthesis' == entry_type:
+            self.degree = "MSc thesis"
+        elif 'phdthesis' == entry_type:
+            self.degree = "PhD thesis"
         self.school = kwargs.get("school", None)
         self.address = kwargs.get("address", None)
-
-        """ path to pdf """
-        self.pdf = kwargs.get("pdf", None)
-
+        
         """ note about entry (e.g., editors' choice, cover article, etc.) """
         self.note = kwargs.get("note", None)
 
         """ date published """
-        self.pub_year = kwargs.get("pub_year", "0001")
-        self.pub_month = kwargs.get("pub_month", "01")
-        self.pub_day = kwargs.get("pub_day", "01")
-        self.pub_date = kwargs.get("pub_date", None)
-
-        """ date submitted """
-        self.sub_year = kwargs.get("sub_year", "0001")
-        self.sub_month = kwargs.get("sub_month", "01")
-        self.sub_day = kwargs.get("sub_day", "01")
-        self.sub_date = kwargs.get("sub_date", None)
-
-        """ convert strings to datetime.date objects """
-        if self.sub_date != None:
-            self.sub_date = datetime.datetime.strptime(self.sub_date, "%Y-%m-%d").date()
-
-        if self.pub_date != None:
-            self.pub_date = datetime.datetime.strptime(self.pub_date, "%Y-%m-%d").date()
-
-        self.date = datetime.date(int(self.year), int(self.month), int(self.day))
-
-    """ number of days between sub_date and pub_date """
-    def days2publish(self):
-        if self.sub_date == None or self.pub_date == None:
-            return None
-        return (self.pub_date - self.sub_date).days
-    
-    """ number of coauthors """
+        self.year = kwargs.get("year", "0001")
+        self.month = kwargs.get("month", "01")
+        
+    # number of coauthors
     def n_authors(self):
         return 1 + self.author.count(",")
         
     def format_thesis(self):
-        if self.kind != "thesis":
-            return ""
+        
         fmt = "<p>" + self.author + "</p>\n"
         # title in quotation marks
         fmt += '<p>"' + self.title + '"</p>\n'
@@ -110,9 +136,12 @@ class bib:
             + self.year
             + ")"
         )
-        if self.openacess == True:
-            fmt += " " + self.icon_openacess
+        
+        # add openaccess symbol
+        if self.openaccess:
+            fmt += " " + self.icon_openaccess
         fmt += "</p>\n"
+    
         # new paragraph if there is stuff to link to
         if self.doi != None or self.arxiv != None:
             fmt += "<p>"
@@ -145,9 +174,9 @@ class bib:
             fmt += "</p>\n"
         return fmt
 
-    def format(self):
+    def format(self,kind='article'):
 
-        if self.kind == "thesis":
+        if kind == "thesis":
             return self.format_thesis()
 
         fmt = "<p>" + self.author + "</p>\n"
@@ -167,8 +196,8 @@ class bib:
                 + self.year
                 + ")"
             )
-            if self.openacess == True:
-                fmt += " " + self.icon_openacess
+            if self.openaccess == True:
+                fmt += " " + self.icon_openaccess
             if self.note != None:
                 fmt += " <mark>" + self.note + "</mark>"
             fmt += "</p>\n"
